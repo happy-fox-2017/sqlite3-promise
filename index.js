92f49898487fe93344a371ba46896099f15b7e80
myyusuf <myyusuf1911@gmail.com>
2017-06-06 14:15:16 +0700

    Release 1

--- index.js
+++ index.js
@@ -3,23 +3,56 @@ const sqlite3 = require('sqlite3').verbose();
 const file = 'data.db';
 const db = new sqlite3.Database(file);
 
-function create(data, callback) {
-  db.run('INSERT INTO teacher VALUES (?, ?, ?)',
-  [data.id, data.name, data.subject], callback);
+function create(data) {
+  return new Promise((resolve, reject) => {
+    db.run('INSERT INTO teacher VALUES (?, ?, ?)',
+    [data.id, data.name, data.subject], (err) => {
+      if (!err) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
 }
 
-function read(callback) {
-  db.all('SELECT * FROM teacher', callback);
+function read() {
+  return new Promise((resolve, reject) => {
+    db.all('SELECT * FROM teacher', (err, rows) => {
+      if (!err) {
+        resolve(rows);
+      } else {
+        reject(err);
+      }
+    });
+  });
 }
 
-function update(data, callback) {
-  db.run('UPDATE teacher SET name = ? WHERE id = ?',
-  [data.name, data.id],
-  callback);
+function update(data) {
+  return new Promise((resolve, reject) => {
+    db.run('UPDATE teacher SET name = ? WHERE id = ?',
+    [data.name, data.id],
+    (err) => {
+      if (!err) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
 }
 
-function deletes(callback) {
-  db.run('DELETE FROM teacher', callback);
+function deletes() {
+  return new Promise((resolve, reject) => {
+    db.run('DELETE FROM teacher',
+    (err) => {
+      if (!err) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
 }
 
 module.exports = {
--- test/test.js
+++ test/test.js
@@ -2,95 +2,102 @@ const assert = require('assert');
 const crud = require('../index.js');
 
 /**
- * contoh testing function dengan callback
- * comment apabila tidak digunakan
- */
-describe('CREATE', function() {
-  it('should invoke callback done', function (done) {
-    crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' }, (err) => {
-      if (!err) {
-        done();
-      } else {
-        done(err);
-      }
-    });
-  })
-});
-
-describe('READ', function() {
-
-  before(function(done) {
-    crud.deletes(() => {
-      crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' }, done);
-    });
-  });
-
-  it('should contains one row', function (done) {
-    crud.read((err, rows) => {
-      if (!err) {
-        assert.equal(1, rows.length);
-        done();
-      } else {
-        done(err);
-      }
-    });
-  })
-});
-
-describe('UPDATE', function() {
-
-  before(function(done) {
-    crud.deletes(() => {
-      crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' }, done);
-    });
-  });
-
-  it('should contains name as Budi', function (done) {
-    crud.update({ id: 1, name: 'Budi' }, (err) => {
-      if (!err) {
-        crud.read((err, rows) => {
-          const row = rows[0];
-          assert.equal('Budi', row.name);
-          done();
-        });
-      } else {
-        done(err);
-      }
-    });
-  })
-});
-
-describe('DELETE', function() {
-
-  before(function(done) {
-    crud.deletes(() => {
-      crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' }, done);
-    });
-  });
-
-  it('should contains no rows', function (done) {
-    crud.deletes((err) => {
-      if (!err) {
-        done();
-      } else {
-        done(err);
-      }
-    });
-  })
-});
-
-/**
  * contoh testing function dengan promise
  * comment apabila tidak digunakan
  */
 describe('CREATE promise', function() {
   it('should resolve and invoke callback done', function(done) {
-    create()
-    .then(function() {
-      done()
+    crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' })
+    .then(function () {
+      done();
     })
-    .catch(function(err) {
-      done(err)
+    .catch(function (err) {
+      done(err);
+    });
+  });
+});
+
+describe('READ promise', function() {
+
+  before(function (done) {
+    crud.deletes()
+    .then(function () {
+      return crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' });
     })
-  })
-})
+    .then(function () {
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+  });
+
+  it('should contains one row', function (done) {
+    crud.read()
+    .then(function (data) {
+      assert.equal(1, data.length);
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+  });
+});
+
+describe('UPDATE promise', function() {
+
+  before(function(done) {
+    crud.deletes()
+    .then(function () {
+      return crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' });
+    })
+    .then(function () {
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+  });
+
+  it('should contains name as Budi', function (done) {
+    crud.update({ id: 1, name: 'Budi' })
+    .then(function () {
+      return crud.read()
+    })
+    .then(function (data) {
+      const row = data[0];
+      assert.equal('Budi', row.name);
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+  });
+});
+
+describe('DELETE promise', function() {
+
+  before(function(done) {
+    crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' })
+    .then(function () {
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+  });
+
+  it('should contains no row', function (done) {
+    crud.deletes()
+    .then(function () {
+      return crud.read()
+    })
+    .then(function (data) {
+      assert.equal(0, data.length);
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+  });
+});
