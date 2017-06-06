b244e3f8b6a6499e53ab554713d824e552904535
myyusuf <myyusuf1911@gmail.com>
2017-06-06 11:42:56 +0700

    Release 0

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function create(data, callback){}
+const sqlite3 = require('sqlite3').verbose();
 
-function read(callback){}
+const file = 'data.db';
+const db = new sqlite3.Database(file);
 
-function update(callback){}
+function create(data, callback) {
+  db.run('INSERT INTO teacher VALUES (?, ?, ?)',
+  [data.id, data.name, data.subject], callback);
+}
 
-function deletes(callback){}
+function read(callback) {
+  db.all('SELECT * FROM teacher', callback);
+}
+
+function update(data, callback) {
+  db.run('UPDATE teacher SET name = ? WHERE id = ?',
+  [data.name, data.id],
+  callback);
+}
+
+function deletes(callback) {
+  db.run('DELETE FROM teacher', callback);
+}
 
 module.exports = {
-  create, read, update, deletes
+  create, read, update, deletes,
 };
--- test/test.js
+++ test/test.js
@@ -1,12 +1,62 @@
-const crud = require('../index.js')
+const assert = require('assert');
+const crud = require('../index.js');
 
 /**
  * contoh testing function dengan callback
  * comment apabila tidak digunakan
  */
 describe('CREATE', function() {
-  it('should invoke callback done', function(done) {
-    create({ id: 1, name: 'John Doe', subject: 'Foo Bar'}, done);
+  it('should invoke callback done', function (done) {
+    crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' }, (err) => {
+      if (!err) {
+        done();
+      } else {
+        done(err);
+      }
+    });
+  })
+});
+
+describe('READ', function() {
+
+  before(function(done) {
+    crud.deletes(() => {
+      crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' }, done);
+    });
+  });
+
+  it('should contains one row', function (done) {
+    crud.read((err, rows) => {
+      if (!err) {
+        assert.equal(1, rows.length);
+        done();
+      } else {
+        done(err);
+      }
+    });
+  })
+});
+
+describe('UPDATE', function() {
+
+  before(function(done) {
+    crud.deletes(() => {
+      crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar' }, done);
+    });
+  });
+
+  it('should contains name as Budi', function (done) {
+    crud.update({ id: 1, name: 'Budi' }, (err) => {
+      if (!err) {
+        crud.read((err, rows) => {
+          const row = rows[0];
+          assert.equal('Budi', row.name);
+          done();
+        });
+      } else {
+        done(err);
+      }
+    });
   })
 })
 
