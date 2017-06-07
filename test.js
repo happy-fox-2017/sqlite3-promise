e42893166b8235377dea44ae998323b7b75a3f29
rusli abdul gani <rusli.gani88@gmail.com>
2017-06-07 10:13:06 +0700

    sqlite3 promise done

---
+++ test.js
@@ -0,0 +1,77 @@
+const crud = require('./index.js')
+
+/**
+ * contoh testing function dengan callback
+ * comment apabila tidak digunakan
+ */
+describe('CREATE promise', function() {
+  it('should invoke callback done', function(done) {
+    crud.create({
+        id: 1,
+        name: 'John Doe',
+        subject: 'Foo Bar'
+      })
+      .then(function() {
+        done()
+      })
+      .catch(function(err) {
+      done(err)
+    })
+  })
+})
+
+describe('READ promise', function() {
+  it('should invoke callback done', function(done) {
+    crud.read()
+      .then(function() {
+        done()
+      })
+      .catch(function(err) {
+      done(err)
+    })
+  })
+})
+
+describe('UPDATE promise', function() {
+  it('should invoke callback done', function(done) {
+    crud.update({
+      id: 1,
+      name: 'Rusli abdul gani',
+      subject: 'Foo Bar'
+    })
+      .then(function() {
+        done()
+      })
+      .catch(function(err) {
+      done(err)
+    })
+  })
+})
+
+describe('DELETE promise', function() {
+  it('should invoke callback done', function(done) {
+    crud.deletes(1)
+      .then(function() {
+        done()
+      })
+      .catch(function(err) {
+      done(err)
+    })
+  })
+})
+
+/**
+ * contoh testing function dengan promise
+ * comment apabila tidak digunakan
+ */
+// describe('CREATE promise', function() {
+//   it('should resolve and invoke callback done', function(done) {
+//     create()
+//       .then(function() {
+//         done()
+//       })
+//       .catch(function(err) {
+//         done(err)
+//       })
+//   })
+// })
